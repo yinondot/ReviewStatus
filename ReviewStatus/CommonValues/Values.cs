ecf4fad8af75b567b21af701b76936ee98625f69
Yinnon Dotan <yinondot@gmail.com>
2018-11-28 17:28:38 +0200

    continuing

---
+++ ReviewStatus/CommonValues/Values.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewStatus.CommonValues
+{
+   public static class Values
+    {
+      public static List<int> NumOfFields =new List<int>() { 1, 2, 3 };
+    }
+}
---
+++ ReviewStatus/ViewModels/MainViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ReviewStatus.CommonValues;
+
+namespace ReviewStatus.ViewModels
+{
+   public class MainViewModel : INotifyPropertyChanged
+   {
+      private ReadOnlyCollection<int> numberOfFields;
+
+      public ReadOnlyCollection<int> NumberOfFields
+      {
+         get { return numberOfFields; }
+         set {
+            if (numberOfFields != value)
+            {
+               numberOfFields = value;
+               OnPropertyChanged();
+            }  
+         }
+      }
+
+     
+      public  MainViewModel()
+      {
+         try
+         {
+          //  InitializeAsync();
+           Task.Factory.StartNew(() => InitializeAsync()) ;
+            Initialize();
+         }
+         catch (Exception ex)
+         {
+
+            System.Windows.MessageBox.Show(ex.Message + " On Initialize");
+         }
+
+      }
+
+      private void Initialize()
+      {
+         
+      }
+
+      private async Task InitializeAsync()
+      {
+        
+         NumberOfFields = new ReadOnlyCollection<int>(Values.NumOfFields);
+        
+      }
+
+     
+
+      private string chosenFile="INITIALIZED.COM";
+
+      public string ChosenFile
+      {
+         get { return chosenFile; }
+         set {
+            if (chosenFile != value)
+            {
+               chosenFile = value;
+               OnPropertyChanged();
+            }
+            
+         }
+      }
+
+
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      private void OnPropertyChanged([CallerMemberName] string propName = "")
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+      }
+
+   }
+}
