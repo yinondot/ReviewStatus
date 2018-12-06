b243bc6c9386bdb2a67bfb5f8dcbeb1b50510362
Yinnon Dotan <yinondot@gmail.com>
2018-12-06 15:42:21 +0200

    sent to be checked as final

--- ReviewStatus/Properties/Resources.Designer.cs
+++ ReviewStatus/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace ReviewStatus.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon IACS_Macro_Icon {
+            get {
+                object obj = ResourceManager.GetObject("IACS_Macro_Icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap iacslogo {
